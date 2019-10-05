d7d4879f6f2d0e0df5735d98b52a6b7669504a18
Robert Dalin <robert.dalin@gmail.com>
2019-10-05 13:24:42 -0500

    add gemfile rake file tests, setup a board string

--- connect-four.rb
+++ connect-four.rb
@@ -1,3 +1,3 @@
 require_relative 'lib/game'
 
-Game.new.start
+g = Game.new.start
--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,38 @@
 # Use this as a sketch of how your Board class could look, you can fill in these
 # methods, or make a different class from scratch
 class Board
+  attr_reader :board_string
   def initialize
+    generate_board
+    puts print_grid
   end
 
   def drop_checker(color, column)
   end
 
-  def print_grid
+  def generate_board
+    @board_string = []
+    6.times do
+      @board_string << empty_row
+    end
+    @board_string << board_bottom
+    @board_string << column_numbers
   end
+
+  def print_grid
+    @board_string.join
+  end
+
+  def empty_row
+    "| | | | | | | |\n"
+  end
+
+  def board_bottom
+    "---------------\n"
+  end
+
+  def column_numbers
+    " 0 1 2 3 4 5 6 \n" end
 
   def game_won?
     false
